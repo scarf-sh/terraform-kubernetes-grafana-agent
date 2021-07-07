a13601265842d7a7401f582bd45edef6f44dec0a
Florian Klink <flokli@flokli.de>
2021-07-07 19:42:54 +0200

    Support configuring pipeline_stage
    This allows configuring another pipeline_stage than the default docker
    one.
    
    This is useful when collecting logs from CRIs other than docker, which
    use the "cri" log format.
    
    See https://github.com/grafana/agent/issues/717#issuecomment-875587164
    for context.

--- modules/grafana-agent-loki/agent-loki.tf
+++ modules/grafana-agent-loki/agent-loki.tf
@@ -7,3 +7,9 @@ variable "loki_password" {
 variable "loki_hostname" {
   type = string
 }
+variable "pipeline_stage" {
+  type        = string
+  default     = "docker"
+  description = "Which pipeline_stage to use"
+  # See https://grafana.com/docs/loki/latest/clients/promtail/configuration/#pipeline_stages for options
+}
