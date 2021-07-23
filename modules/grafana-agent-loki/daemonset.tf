5109be7b385cf6f1a2c2d74ac00a155744c8a7e6
Florian Klink <flokli@flokli.de>
2021-07-23 16:06:47 +0200

    grafana-agent-loki: update to config from Grafana Agent Logs Kubernetes Quickstart

--- modules/grafana-agent-loki/daemonset.tf
+++ modules/grafana-agent-loki/daemonset.tf
@@ -5,7 +5,7 @@ resource "kubernetes_config_map" "grafana_agent_logs" {
   }
 
   data = {
-    "agent.yaml" = templatefile("${path.module}/agent-daemonset.yaml", {
+    "agent.yaml" = templatefile("${path.module}/agent.yaml", {
       LOKI_USERNAME  = var.loki_username
       LOKI_HOSTNAME  = var.loki_hostname
       pipeline_stage = var.pipeline_stage
